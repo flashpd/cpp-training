a8bf0f2fea0016845d4a5432585978346f7c6b60
stop-coding <2470977889@qq.com>
2020-06-04 16:27:26 +0800

    Create 1-1.cpp

---
+++ CH1/1-1.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Welcome to C++" << endl
+        << "Welcome to Computer Science" << endl
+        << "Programming is fun" << endl;
+
+    return 0;
+}
